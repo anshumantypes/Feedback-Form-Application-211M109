ce21bf3335d6405da477d8b5c96386bec689bc2b
anshuman upadhyay <anshumanupadhyay.211me109@nitk.edu.in>
2022-10-25 13:03:14 +0530

    created migrations

--- feedback/func/models.py
+++ feedback/func/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class contactform(models.Model):
+    fname = models.CharField(max_length=25)
+    lname = models.CharField(max_length=25)
+    email = models.EmailField(max_length=50)
+    phone = models.IntegerField()
+    query = models.TextField()
--- feedback/func/views.py
+++ feedback/func/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import contactform
 
 # Create your views here.
 def mailer(request):
-    return render(request,'feedback.html')
+    if request.method=='POST':
+        fname=request.POST['first']
+        lname=request.POST['last']
+        email=request.POST['mail']
+        phone=request.POST['number']
+        query=request.POST['query']
+
+        return render(request,'feedback.html',{'message':fname})
+    else:
+        return render(request,'feedback.html',{})
